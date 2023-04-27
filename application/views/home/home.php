409183098a5b7c0a2748cb1c0033afbc456b8813
Abelardjohn <abelardpangalila@gmail.com>
2023-04-28 02:10:33 +0800

    first commit

---
+++ application/views/home/home.php
@@ -0,0 +1,344 @@
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div id="logo">
+        <h1><a href="index.html"><span>MSM</span>project</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about-us">About</a></li>
+          <li><a class="nav-link scrollto" href="#features">Features</a></li>
+          <li><a class="nav-link scrollto" href="#screenshots">Screenshots</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+          <li><a class="nav-link scrollto active" href="#contact"><strong>LOGIN</strong></a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <hr>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Welcome to (nama perusahaan)</h1>
+      <h2>boleh berisi tagline website</h2>
+      <img style="width: 400px; height: 300px"  src="<?= base_url('/assets/img/hero2.jpg') ?> " alt="Hero Imgs" data-aos="zoom-out" data-aos-delay="100">
+      
+      <a href="#get-started" class="btn-get-started scrollto">Get Started</a>
+      <div class="btns">
+        <a href="#"><i class="fa fa-apple fa-3x"></i> <i class="bi bi-facebook"></i> facebook</a>
+        <a href="#"><i class="fa fa-play fa-3x"></i> <i class="bi bi-instagram"></i> instagram</a>
+        <a href="#"><i class="fa fa-windows fa-3x"></i> <i class="bi bi-tiktok"></i> tiktok</a>
+      </div>
+    </div>
+  </section><!-- End Hero Section -->
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+      <div class="container">
+
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-4">
+            <img  src="<?= base_url('/assets/img/coconut-1.png') ?>" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-5 content">
+            <h2>Tentang Perusahaan</h2>
+            <p class="fst-italic py-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+            <p class="py-3">
+            
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+
+   <!-- ======= produk Section ======= -->
+   <section id="team" class="padd-section text-center">
+
+<div class="container" data-aos="fade-up">
+  <div class="section-title text-center">
+
+    <h2>FOTO PRODUK</h2>
+    <p class="separator">(menampilkan foto produk terbaik)</p>
+  </div>
+
+  <div class="row">
+
+    <div class="col-md-6 col-md-4 col-lg-3" data-aos="zoom-in" data-aos-delay="100">
+      <div class="team-block bottom">
+        <img src="<?= base_url('/assets/img/coconut-1.png') ?>" class="img-responsive" alt="img">
+        <div class="team-content">
+          <span>coconut</span>
+          <h4>coconut oil</h4>
+          <ul class="list-unstyled">
+            <p>deskripsi singkat produk </p>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-md-4 col-lg-3" data-aos="zoom-in" data-aos-delay="200">
+      <div class="team-block bottom">
+        <img src="<?= base_url('/assets/img/coconut-2.png') ?>" class="img-responsive" alt="img">
+        <div class="team-content">
+        <span>coconut</span>
+          <h4>coconut oil</h4>
+          <ul class="list-unstyled">
+            <p>deskripsi singkat produk </p>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-md-4 col-lg-3" data-aos="zoom-in" data-aos-delay="300">
+      <div class="team-block bottom">
+        <img src="<?= base_url('/assets/img/coconut-1.png') ?>" class="img-responsive" alt="img">
+        <div class="team-content">
+        <span>coconut</span>
+          <h4>coconut oil</h4>
+          <ul class="list-unstyled">
+            <p>deskripsi singkat produk </p>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-md-4 col-lg-3" data-aos="zoom-in" data-aos-delay="400">
+      <div class="team-block bottom">
+        <img src="<?= base_url('/assets/img/coconut-1.png') ?>" class="img-responsive" alt="img">
+        <div class="team-content">
+        <span>coconut</span>
+          <h4>coconut oil</h4>
+          <ul class="list-unstyled">
+            <p>deskripsi singkat produk </p>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+</section><!-- End produk Section -->
+
+  <!-- ======= service Section ======= -->
+  <section id="get-started" class="padd-section text-center">
+
+<div class="container" data-aos="fade-up">
+  <div class="section-title text-center">
+
+    <h2>OUR SERVICE</h2>
+    <p class="separator">Integer cursus bibendum augue ac cursus .</p>
+
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="100">
+      <div class="feature-block">
+
+        <img src="<?= base_url('/assets/img/svg/cloud.svg') ?>" alt="img">
+        <h4>introducing whatsapp</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        <a href="#">read more</a>
+
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="200">
+      <div class="feature-block">
+
+        <img src="<?= base_url('/assets/img/svg/planet.svg') ?>" alt="img">
+        <h4>user friendly interface</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        <a href="#">read more</a>
+
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="300">
+      <div class="feature-block">
+
+        <img src="<?= base_url('/assets/img/svg/asteroid.svg') ?>" alt="img">
+        <h4>build the app everyone love</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        <a href="#">read more</a>
+
+      </div>
+    </div>
+
+  </div>
+</div>
+
+</section><!-- End service Section --> 
+
+  <!-- ======= Pricing Section ======= -->
+<section id="pricing" class="pricing">
+      <div class="container">
+
+        <div class="section-header">
+          <h2>Prices</h2>
+          <!-- <p>Check my adorable pricing</p> -->
+        </div>
+
+        <div class="row gy-4 gx-lg-5">
+
+          <div class="col-lg-6">
+            <div class="pricing-item d-flex justify-content-between">
+              <p>coconut</p>
+              <h4>$160.00</h4>
+            </div>
+          </div><!-- End Pricing Item -->
+
+          <div class="col-lg-6">
+            <div class="pricing-item d-flex justify-content-between">
+              <p>coconut</p>
+              <h4>$300.00</h4>
+            </div>
+          </div><!-- End Pricing Item -->
+
+          <div class="col-lg-6">
+            <div class="pricing-item d-flex justify-content-between">
+              <p>coconut</p>
+              <h4>$200.00</h4>
+            </div>
+          </div><!-- End Pricing Item -->
+
+          <div class="col-lg-6">
+            <div class="pricing-item d-flex justify-content-between">
+              <p>coconut</p>
+              <h4>$120.00</h4>
+            </div>
+          </div><!-- End Pricing Item -->
+
+          <div class="col-lg-6">
+            <div class="pricing-item d-flex justify-content-between">
+              <p>coconut</p>
+              <h4>$500.00</h4>
+            </div>
+          </div><!-- End Pricing Item -->
+
+          <div class="col-lg-6">
+            <div class="pricing-item d-flex justify-content-between">
+              <p>coconut</p>
+              <h4>$200.00</h4>
+            </div>
+          </div><!-- End Pricing Item -->
+
+        </div>
+    </section><!-- End Pricing Section -->
+
+    <br><br>
+
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 col-lg-4">
+          <div class="footer-logo">
+
+            <a class="navbar-brand" href="#">eStartup</a>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Abou Us</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Features item</a></li>
+              <li><a href="#">Live streaming</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Abou Us</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Features item</a></li>
+              <li><a href="#">Live streaming</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Support</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">faq</a></li>
+              <li><a href="#">Editor help</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-2">
+          <div class="list-menu">
+
+            <h4>Abou Us</h4>
+
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Features item</a></li>
+              <li><a href="#">Live streaming</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </footer><!-- End  Footer -->
---
+++ application/views/template/footer.php
@@ -0,0 +1,19 @@
+ 
+ 
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="<?= base_url('/vendor/aos/aos.js') ?>"></script>
+<script src="<?= base_url('/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('/vendor/glightbox/js/glightbox.min.js') ?>"></script>
+<script src="<?= base_url('/vendor/swiper/swiper-bundle.min.js') ?>"></script>
+<script src="<?= base_url('/vendor/php-email-form/validate.js') ?>"></script>
+
+<script src="<?= base_url('/assets/main.js') ?>"></script>
+
+
+</body>
+</html>
